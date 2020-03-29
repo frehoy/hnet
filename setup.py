bda15fd3f0446a669813eae239eb773f827a5ea7
frehoy <frederick.hoyles@pcr.uu.se>
2020-03-29 17:13:46 +0200

    reduced python version requirement to 3.7 because getting 3.8 in ubuntu is painful

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     )
 
